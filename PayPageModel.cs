5a926abebae48020599fdcace32a9fc98f21e917
macwinandios <105177727+macwinandios@users.noreply.github.com>
2022-05-09 19:18:42 -0400

    Add files via upload
    MAY 9, 2022
    I Need To:
    
    1. PAYPAGEMODEL: Calculate the pay amount according to how many hours the user has clocked in if they earn $20US each hour.
    2. PAYPAGEMODEL: Display the current week with the total hours and total pay for the week. (Week: May 1 - May 7  Pay: $553.95 TotalHours: 39.50) This is just an example.
    3.PAYPAGEMODEL: Connect the Observable Collection so it can update this page too.
    4.PAYPAGEMODEL: Each index can be updated/changed by a manager (Week, Pay, TotalHours).
    5.CLOCKINPAGEMODEL: Add a label to the ClockinPage that will update the TotalHours dynamically each time the user clocks out. (Maybe by adding each [0] index of the Observable Collection's Total hours to itself). So if they clock in for 5 seconds and clock out.  Then they clock in again for 10 seconds and clock out.  This label will display- Total Hours: 15 seconds.

---
+++ PayPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Customized.Model;
+using Xamarin.Forms;
+
+namespace Customized.PageModel
+{
+    public class PayPageModel : ClockinPageModel
+    {
+
+        public PayPageModel()
+        {
+
+
+        }
+    }
+}
